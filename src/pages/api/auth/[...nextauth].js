cce12b3d6c5562826c0f8eaf42f5c27cf8580598
Koray Can Yılmaz <krycnylmz@gmail.com>
2024-05-23 12:45:15 +0300

    next-auth version provider bug fix

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,12 +1,11 @@
 import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
 import connectToDatabase from '../../../lib/mongodb';
 import User from '../../../models/User';
 
 import GoogleProvider from "next-auth/providers/google"
 export default NextAuth({
   providers: [
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
